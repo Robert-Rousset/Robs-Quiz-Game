ebee8e51d96e05df85d19b0be86e28d8fff848e2
Robert Rousset <whyisrob@hotmail.com>
2021-03-28 22:07:34 +0800

    fairly well functioning app, needs minor improvements

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ var questions = ['Which art movement is Salvador Dali associated with?',
 var correctAnswers = ['Surrealism', 'Neptune', 'Tropical Orchid', 'Avatar', '32', 'John Lennon', 'Rice', 'A Smack of Jellyfish', 'TikTok', 'Falcon']
 var wrongAnswers1 = ['Impressionism', 'Jupiter', 'Vanillious', 'Avengers: Endgame', '28', 'Ringo Star', 'Potato', 'A Gaggle of Jellyfish', 'WhatsApp', 'Cheetah']
 var wrongAnswers2 = ['Cubism', 'Mercury', 'Sugar Cane', 'Titalic', '34', 'Paul McCartney', 'Beans', 'A Clutter of Jellyfish', 'Instagram', 'Black Marlin']
-var wrongAnswers3 = ['Abstract Art', 'Uranus', 'Butter Lily', 'Star Wars: The Force Awakens', '30', 'George Harrison', 'Cherry Blossom', 'A Gloop of Jellyfish', 'Zoom', 'Usain Bolt']
-var ul = document.querySelector('ul')
-var card = document.querySelector('.card')
+var wrongAnswers3 = ['Abstract Art', 'Uranus', 'Butter Lily', 'The Lion King', '30', 'George Harrison', 'Cherry Blossom', 'A Gloop of Jellyfish', 'Zoom', 'Usain Bolt']
+var answers = document.querySelector('.answers')
+var enterScore = document.querySelector('.enterscore')
+var scoreBoard = document.querySelector('.scoreboard')
+var score = document.querySelector('#score')
 var answerA = document.querySelector('.A')
 var answerB = document.querySelector('.B')
 var answerC = document.querySelector('.C')
@@ -23,10 +25,18 @@ var questionNumber = document.querySelector('.questionNumber')
 var theQuestion = document.querySelector('p')
 var start = document.querySelector('.start')
 var hide = document.querySelector('#hide')
+var h2 = document.querySelector('h2')
+var h3 = document.querySelector('h3')
+var h4 = document.querySelector("h4") 
+var button = document.querySelector('button')
+var li = document.querySelector('li')
+var ol = document.querySelector('ol')
+var playAgain = document.querySelector('.playAgain')
+
 var index = 0;
-var seconds = 100;
+var scoreRanking = 0;
+var seconds = 60;
 var Qnum = 1;
-var timerInt = 0;
 
 //START BUTTON//
 start.textContent = "START";
@@ -35,59 +45,106 @@ start.addEventListener("click", function(){
     hide.style.display = "block";
     printText();
     start.style.display = "none";
+    timeLeft();
 })
 
-
+function resetVariables(){
+    hide.style.display = "block";
+    index = 0;
+    seconds = 60;
+    Qnum = 1;
+    h4.style.display = 'none';
+    h2.style.display = "block";
+    printText();
+    timeLeft();
+};
 
 //ANSWER SELECTION CORRECT OR INCORRECT//
-ul.addEventListener("click", function(event) {
+answers.addEventListener("click", function(event) {
     var element = event.target.innerHTML;
     if (element === correctAnswers[index]){
         nextQuestion();
-    }else {
-        seconds--;
+        h3.style.color = 'green';
+        h4.style.display = 'block';
+        h4.style.color = 'green';
+        h4.innerHTML = "CORRECT";
+    }else if (element === wrongAnswers1 || wrongAnswers2 || wrongAnswers3){
+        h3.style.color = 'red';
+        seconds-=5;
         nextQuestion();
+        h4.style.display = 'block';
+        h4.innerHTML = "INCORRECT";
+        h4.style.color = 'red';
     }
-    console.log(element)
   });
 
-
-
-
-  function printText(){
+function printText(){
     questionNumber.textContent = "QUESTION " + Qnum;
     theQuestion.textContent = questions[index];
     answerA.textContent = correctAnswers[index];
     answerB.textContent = wrongAnswers1[index];
     answerC.textContent = wrongAnswers2[index];
     answerD.textContent = wrongAnswers3[index];
-
-    for (let i = ul.children.length; i >= 0; i--) {
-        ul.appendChild(ul.children[Math.random() * i | 0])        
-    }
+    for (let i = answers.children.length; i >= 0; i--) {
+        answers.appendChild(answers.children[Math.random() * i | 0])        
+    }}
 
 //TIMER SET//
-var timerInt = setInterval(function() {
-    seconds--;
-    secondsOnClock.textContent = seconds + " seconds left";   
-    if(seconds <= 0) {
-    clearInterval(timerInt);
-    gameOver();
+function timeLeft(){
+    var timerInt = setInterval(function() { 
+        h3.style.color = 'black';
+        seconds--;
+        secondsOnClock.textContent = seconds + " seconds left"; 
+        if (index >= questions.length){
+            clearInterval(timerInt);
+            gameOver();
+        }
+        if(seconds <= 0) {
+        clearInterval(timerInt);
+        gameOver();
 }}, 1000);}
 
 function nextQuestion(){
     index ++;
     Qnum ++;
-    clearInterval(timerInt);
     printText();
-
- 
 }
 
 function gameOver(){
-
+    h4.style.display = "none";
+    answers.style.display = "none";
+    questionNumber.style.display = "none";
+    hide.style.display = "none";
+    enterScore.style.display = "flex";
+    scoreBoard.style.display = 'block';
+    h2.style.display = "block";
+    playAgain.style.display = "flex";
+    playAgain.textContent = "Submit";
+    playAgain.addEventListener('click', function(){
+        playAgain.textContent = "Play Again?";
+        li.textContent = score.value + " - " + seconds + " seconds left";
+        li.style.textAlign = 'center';
+        playAgain.addEventListener('click', function(){
+            h4.style.display = "block";
+            answers.style.display = "block";
+            questionNumber.style.display = "block";
+            hide.style.display = "block";
+            enterScore.style.display = "none";
+            scoreBoard.style.display = 'none';
+            h2.style.display = "none";
+            playAgain.style.display = "none";
+            resetVariables();
+        })
+    })
 }
 
-function Points(){
-
-}
+enterScore.addEventListener('keydown', function(event){
+    scoreRanking++;
+    localStorage.setItem("userScore", score.value);
+    console.log(event.key)
+    if (event.key === 'Enter'){
+        playAgain.textContent = "Play Again?"
+        li.textContent = score.value + " - " + seconds + " seconds left";
+        li.style.textAlign = 'center';;
+    }
+})
